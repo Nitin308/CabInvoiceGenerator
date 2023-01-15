7a7aa1aeb48f7955c611488e41c1e4332bfe15d9
Nitin308 <nitin3081998@gmail.com>
2023-01-15 12:38:16 +0530

    introduce premium rides

---
+++ CabInvoiceGenerator/CabInvoiceGenerator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabInvoiceGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            Console.WriteLine(invoiceGenerator.CalculateFare(5, 6));
+            Ride[] rides = { new Ride(3, 25), new Ride(0.5, 10) };
+            InvoiceSummary actual = invoiceGenerator.CalculateFare(rides);
+            Console.ReadLine();
+        }
+    }
+}
