d300d4a0abd78c0ad73d339e47b518540c99bb7e
Nitin308 <nitin3081998@gmail.com>
2023-01-15 10:43:32 +0530

    Add CabInvoiceGenerator Problem solution

---
+++ CabInvoiceGenerator/InvoiceGenerator.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabInvoiceGenerator
+{
+    public class InvoiceGenerator
+    {
+        /// <summary>
+        /// Declaring some variable
+        /// </summary>
+        private readonly double MINIMUM_COST_PER_KM;
+        private readonly double COST_PER_TIME;
+        private readonly double MINIMUM_FARE;
+        /// <summary>
+        /// Creating a constructor
+        /// </summary>
+        public InvoiceGenerator()
+        {
+            this.MINIMUM_COST_PER_KM = 10;
+            this.COST_PER_TIME = 1;
+            this.MINIMUM_FARE = 5;
+        }
+        /// <summary>
+        /// Creating method for calculating Totalfare based on distance and time
+        /// </summary>
+        /// <param name="distance"></param>
+        /// <param name="time"></param>
+        /// <returns></returns>
+        public double CalculateFare(double distance, int time)
+        {
+            double totalFare = 0;
+            try
+            {
+                if (distance <= 0)
+                {
+                    throw new CabInvoiceException(CabInvoiceException.ExceptionType.INVALID_DISTANCE, "Invalid Distance");
+                }
+                if (time <= 0)
+                {
+                    throw new CabInvoiceException(CabInvoiceException.ExceptionType.INVALID_DISTANCE, "Invalid Time");
+                }
+
+                totalFare = distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME;
+            }
+            catch (CabInvoiceException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return Math.Max(totalFare, MINIMUM_FARE);
+        }
+        /// <summary>
+        /// Creating method for calculating Totalfare, number of rides and average totalfare of multiple rides based on distance and time
+        /// </summary>
+        /// <param name="rides"></param>
+        /// <returns></returns>
+        /// <exception cref="CabInvoiceException"></exception>
+        public InvoiceSummary CalculateFare(Ride[] rides)
+        {
+            double totalFare = 0;
+            try
+            {
+                foreach (Ride ride in rides)
+                {
+                    totalFare += CalculateFare(ride.distance, ride.time);
+                }
+            }
+            catch (Exception)
+            {
+                throw new CabInvoiceException(CabInvoiceException.ExceptionType.NULL_RIDE, "Ride should not be null");
+            }
+            double result = Math.Max(totalFare, MINIMUM_FARE);
+            return new InvoiceSummary(result, rides.Length);
+        }
+    }
+}
---
+++ CabVoiceGeneratorTesting/Test.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CabInvoiceGenerator;
+using System;
+
+namespace CabInvoiceGeneratorTesting
+{
+    [TestClass]
+    public class Test
+    {
+        /// <summary>
+        /// For Single ride Only
+        /// </summary>
+        [TestMethod]
+        public void GivenDistanceAndTimeShouldReturnsTotalFare()
+        {
+            //Arrange
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+            double distance = 5;
+            int time = 6;
+            double expected = 56;
+            //Act
+            double actual = invoiceGenerator.CalculateFare(distance, time);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        /// <summary>
+        /// For multiple rides
+        /// </summary>
+        //[TestMethod]
+        //public void GivenMultipleRidesShouldReturnsAggregateTotalFare()
+        //{
+        //    //Arrange
+        //    InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        //    Ride[] rides = { new Ride(3,25),new Ride(0.5,10) };
+        //    double expected = 70;
+        //    //Act
+        //    double actual = invoiceGenerator.CalculateFare(rides);
+        //    //Assert
+        //    Assert.AreEqual(expected, actual);
+        //}
+        ///
+        /// <summary>
+        /// Given distance and time of multiple rides should rerurns Totalfare, number of rides and average totalfare
+        /// </summary>
+        [TestMethod]
+        public void GivenMultipleRidesShouldReturnsAggregateTotalFareWithAverage()
+        {
+            //Arrange
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+            Ride[] rides = { new Ride(3, 25), new Ride(0.5, 10) };
+            InvoiceSummary expected = new InvoiceSummary(70, rides.Length);
+            //Act
+            InvoiceSummary actual = invoiceGenerator.CalculateFare(rides);
+            //Assert
+            Assert.AreEqual(expected, actual);
+            //expected.Equals(actual);
+        }
+    }
+
+}
